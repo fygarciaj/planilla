36493c1942b8125d3f10c92522817049319d910d
Fabian Garcia <fygarciaj@gmail.com>
2023-05-09 16:58:55 -0500

    avanced

--- src/Components/InputNumericCalc.tsx
+++ src/Components/InputNumericCalc.tsx
@@ -1,7 +1,8 @@
 import { faDeleteLeft } from "@fortawesome/free-solid-svg-icons";
 import { Button, Form, ListGroup } from "solid-bootstrap";
 import Fa from "solid-fa";
-import { Component, For, createEffect, createSignal } from "solid-js";
+import { Component, For, Show, createEffect, createSignal } from "solid-js";
+import { createStore } from "solid-js/store";
 
 const InputNumericCalc: Component = (props) => {
 
@@ -11,6 +12,8 @@ const InputNumericCalc: Component = (props) => {
 
     const [totalAcumulate, setTotalAcumulate] = createSignal(0)
     const [acumulateList, setAcumulateList] = createSignal([] as itemValue[])
+    const [showList, setShowList] = createSignal(false)
+    const storeId = createStore()
 
     function addDataAcumulate(e: KeyboardEvent & { currentTarget: HTMLInputElement | HTMLTextAreaElement; target: Element; }) {
         //console.log(e)
@@ -20,42 +23,51 @@ const InputNumericCalc: Component = (props) => {
             }
 
             setAcumulateList([newAcumulate, ...acumulateList()])
-         //   console.log(acumulateList(), e.target)
+            //   console.log(acumulateList(), e.target)
             e.target.value = ''
         }
     }
 
     createEffect(() => {
         const total = acumulateList().reduce((a, b) => a + b.value, 0)
-       // console.log(total)
+        // console.log(total)
         setTotalAcumulate(total)
     })
 
-    function delDataAcumulate(value: number): void {
-        console.log(value)
+    const delDataAcumulate = (index: number) => {
+        const nuevosElementos = [...acumulateList()];
+        nuevosElementos.splice(index, 1);
+        setAcumulateList(nuevosElementos);
+        console.log(index)
+    }
+
+    const handleShowList = () => {
+        setShowList(!showList())
     }
 
     return (
         <>
             <Form.Group>
-                <Form.Label  for="numericInputCalc">{props.label}</Form.Label>
-                <Form.Control type="number" inputMode="numeric" placeholder="0.00" onKeyPress={(e) => { addDataAcumulate(e) }} id="numericInputCalc"/> 
+                <Form.Label for="numericInputCalc">{props.label}</Form.Label>
+                <Form.Control type="number" inputMode="numeric" placeholder="0.00" onKeyPress={(e) => { addDataAcumulate(e) }} id="numericInputCalc" />
                 <Form.Control.Feedback type="invalid">
                     Please enter a number
                 </Form.Control.Feedback>
             </Form.Group>
             <ListGroup variant="flush">
                 <For each={acumulateList()}>
-                    {(acumulate) =>
+                    {(acumulate, index) =>
                         <>
-                            <ListGroup.Item class="d-flex justify-content-end align-items-center" action onClick={delDataAcumulate(acumulate.value)}>
-                                {acumulate.value} 
-                                <Button ></Button><Fa icon={faDeleteLeft}></Fa>
-                            </ListGroup.Item>
+                            <Show when={!showList()}>
+                                <ListGroup.Item class="d-flex justify-content-end align-items-center" action onClick={delDataAcumulate(index)}>
+                                    {acumulate.value}
+                                    <Fa icon={faDeleteLeft}></Fa>
+                                </ListGroup.Item>
+                            </Show>
                         </>
                     }
                 </For>
-                <ListGroup.Item class="d-flex justify-content-end align-items-center">
+                <ListGroup.Item class="d-flex justify-content-end align-items-center" onClick={handleShowList}>
                     Total: {totalAcumulate()}
                     <Fa icon={faDeleteLeft}></Fa>
                 </ListGroup.Item>
